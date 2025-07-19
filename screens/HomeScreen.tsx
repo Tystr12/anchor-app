4a6dc3036c4c1d6b7aacff45c24e2e8bbf42d353
Tystr12 <ty.strong1212@gmail.com>
2025-07-20 00:32:13 +0200

    added cool orbs. very nice.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Animated } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { useNavigation } from '@react-navigation/native';
@@ -9,10 +9,33 @@ import type { RootStackParamList } from '../types/navigation';
 export default function HomeScreen() {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
+  // Breathing orb animation
+  const pulseAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(pulseAnim, {
+          toValue: 1.2,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(pulseAnim, {
+          toValue: 1,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, []);
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Text style={styles.welcome}> Welcome to Anchor ⚓️</Text>
       <ScrollView contentContainerStyle={styles.content}>
+        <Animated.View style={[styles.orbOutline, { transform: [{ scale: pulseAnim }] }]}>
+          <Animated.View style={[styles.orb, { transform: [{ scale: pulseAnim }] }]} />
+        </Animated.View>
+        <Text style={styles.welcome}>Welcome to Anchor ⚓️</Text>
         <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('PanicHelp')}>
           <Text style={styles.buttonText}>I’m Feeling Anxious</Text>
         </TouchableOpacity>
@@ -50,7 +73,8 @@ const styles = StyleSheet.create({
   color: '#003049',
 },
   content: {
-    paddingTop: 80,
+    flexGrow: 1,
+    paddingTop: 140,
     paddingBottom: 40,
     paddingHorizontal: 20,
     alignItems: 'center',
@@ -58,6 +82,7 @@ const styles = StyleSheet.create({
   greeting: {
     fontSize: 20,
     marginBottom: 40,
+    paddingTop: 20,
     textAlign: 'center',
     color: '#333',
   },
@@ -75,5 +100,21 @@ const styles = StyleSheet.create({
     color: '#003049',
     fontSize: 16,
     fontWeight: '600',
+  },
+  orb: {
+    width: 110,
+    height: 110,
+    borderRadius: 55,
+    backgroundColor: '#b3e5fc',
+  },
+  orbOutline: {
+    width: 130,
+    height: 130,
+    borderRadius: 65,
+    borderWidth: 5,
+    borderColor: '#c6f6c4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 40,
   },
 });
